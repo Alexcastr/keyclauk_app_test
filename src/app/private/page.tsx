715c090297e56e4c77b1994f323c239ff402fded
alexander <castro.t.alex@gmail.com>
2024-09-21 20:15:20 -0500

    working on authorization

--- src/app/private/page.tsx
+++ src/app/private/page.tsx
@@ -1,8 +1,20 @@
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { roles } from "../constants/constants";
+import { redirect } from 'next/navigation';
 
-const PrivatePage = () => {
+const PrivatePage = async () => {
+
+  const session = await getServerSession(authOptions);
+
+  if(session?.roles?.includes(roles.EMPLOYEE)) {
+    // toast.error("You are not authorized to view this page")
+    redirect("/");
+  }
   return (
     <div>
       <h1> PrivatePage</h1>
+      <p> This is a private page only the admin can enter here </p>
     </div>
     
   )
--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,7 +1,33 @@
 "use client"
-import { signOut } from "next-auth/react";
+import { useSession,  signOut } from "next-auth/react";
+import { useEffect } from "react";
+
+async function keycloakSessionLogOut() {
+    try {
+      await fetch(`/api/auth/logout`, { method: "GET" });
+    } catch (err) {
+      console.error(err);
+    }
+  }
 export default function Logout() {
-  return <button onClick={() => signOut()}>
-    Signout of keycloak
+
+  const { data: session, status } = useSession(); 
+
+  useEffect(() => {
+    
+    if (
+      status != "loading" &&
+      session &&
+      session?.error === "RefreshAccessTokenError"
+    ) {
+      signOut({ callbackUrl: "/" });
+    }
+  }, [session, status]);
+  return <button 
+  className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
+  onClick={() => {
+    keycloakSessionLogOut().then(() => signOut({ callbackUrl: "/" }));
+  }}>
+    Signout
   </button>
 }
